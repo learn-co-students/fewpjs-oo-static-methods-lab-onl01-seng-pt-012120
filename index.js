588df509932538906eb656ddd62fa59ca9951df6
github username <github email address>
2020-08-24 20:30:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-class Formatter {
-  //add static methods here
+// class Formatter {
+//   //add static methods here
+// }
+
+
+class Formatter  {
+  static capitalize(string) {
+    // capitalizes first letter
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, '');
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const wordArray = string.split(" ")
+    
+    const result = wordArray.map(function(word, i) {
+    
+      return (!exceptions.includes(word)) || i === 0 ? this.capitalize(word) : word
+    }, this)
+    
+    return result.join(" ")
+  }
 }
+// }
+  
